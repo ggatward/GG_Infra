fd034e6c6f74cd40434d2725d987189ca2e5f98a
Geoff Gatward <geoff@shack.core.home.gatwards.org>
2019-09-23 08:03:36 +1000

    OpenHab tweaks

--- templates/oh_templates/transform/cm-to-mm.js
+++ templates/oh_templates/transform/cm-to-mm.js
@@ -1,10 +1,12 @@
 (function(i) {
-  // Items come into the trasnform as strings - we need to parse it as a float
-  var cm = parseFloat(i);
-  // Perform the math and strip to 1 decimal place
-  var mm = (cm * 10).toFixed(1);
-  // Append the text string for units
-  var returnString = mm + ' mm';
-  // And return the result back to the item
-  return returnString.trim();
+    // Enable logging - log using logger.warn(....) - goes to openhab2.log
+    var logger = Java.type("org.slf4j.LoggerFactory").getLogger("cm-to-mm.js");
+
+    // Items come into the trasnform as strings - we need to parse it as a float
+    var cm = parseFloat(i);
+    // Perform the math
+    var mm = (cm * 10);
+    // And return the result back to the item (still as a float var)
+    //logger.warn(mm);
+    return mm;
 })(input)
